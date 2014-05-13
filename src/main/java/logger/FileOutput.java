a6d0876c41425a033595d6ca8c841ab858dab276
Valentino Portnoy <vnportnoy@gmail.com>
2014-05-12 23:45:29 -0300

    FileOutput tests

--- src/main/java/logger/FileOutput.java
+++ src/main/java/logger/FileOutput.java
@@ -4,13 +4,11 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.InvalidPathException;
 
 public class FileOutput implements Writable {
 	
 	private String path;
-	private FileWriter fileWriter;
 	public static final String NULL_PATH = "Path given is NULL";
 	public static final String WRONG_PATH = "Path given does not exist";
 	
@@ -22,7 +20,7 @@ public class FileOutput implements Writable {
 		return path;
 	}
 
-	public void setPath(String path) throws IOException {
+	private void setPath(String path) throws IOException {
 		if (path == null) {
 			throw new InvalidPathException(path,NULL_PATH);
 		}
@@ -30,15 +28,21 @@ public class FileOutput implements Writable {
 		File file = new File(path);
 		if (!file.exists()) {
 			file.createNewFile();
-		}
-		this.fileWriter = new FileWriter(file,true);
+		}			
 	}
 	
-	public void write(String text) {		
-  	  	BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-  	  	PrintWriter printWriter = new PrintWriter(bufferedWriter);
-		printWriter.println(text);
-  	  	printWriter.close();
+	// TODO: Handle exception without modifying interface
+	public void write(String text) {        
+        try {
+        	FileWriter fileWriter = new FileWriter(path,true);
+        	BufferedWriter writer = new BufferedWriter(fileWriter);
+			writer.append(text);
+			writer.newLine();
+	        writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
 	}
 
 }
---
+++ src/test/java/TestFile.java
@@ -0,0 +1,66 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import main.java.logger.FileOutput;
+import main.java.logger.Writable;
+
+import org.junit.Test;
+
+public class TestFile {
+	
+	private boolean stringInFile(String text, File file) throws FileNotFoundException {
+		Scanner fileScanner = new Scanner(file);
+		boolean found = false;
+		while(fileScanner.hasNextLine()){
+		     if(text.equals(fileScanner.nextLine().trim())){
+		        found = true;
+		        break;
+		      }
+		 }
+		fileScanner.close();
+		return found;
+	}
+
+	@Test
+	public void testWriteOneString() throws IOException {
+		String path = "log.txt";
+		String text = "test";
+		File file = new File(path);
+		file.delete();
+		Writable fileOutput = new FileOutput(path);
+		fileOutput.write(text);		
+		assertTrue(stringInFile(text,file));
+	}
+	
+	@Test
+	public void testWriteTwoStrings() throws IOException {
+		String path = "log.txt";
+		File file = new File(path);
+		file.delete();
+		String text1 = "test1";
+		String text2 = "test2";
+		Writable fileOutput = new FileOutput(path);
+		fileOutput.write(text1);
+		fileOutput.write(text2);
+		assertTrue(stringInFile(text1,file));
+		assertTrue(stringInFile(text2,file));
+	}
+	
+	@Test
+	public void testNullFile() throws IOException {
+		try {
+			new FileOutput(null);
+		} catch (Exception e) {
+			assertTrue(true);
+			return;
+		}
+		assertTrue(false); // should never reach here
+	}
+
+}
