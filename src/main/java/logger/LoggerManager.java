e342926007a6f5e15053ff2cb97532e710b407df
Docente <alumno@Exomate-X352.Exomate>
2014-05-15 16:38:17 -0300

    Changed dependencies of LoggerManager

--- src/main/java/logger/LoggerManager.java
+++ src/main/java/logger/LoggerManager.java
@@ -4,41 +4,41 @@ import java.io.IOException;
 
 
 public class LoggerManager {
-
-	ConfigurationLoader configLoader;
-	Formatter formatter;
+	
 	Logger logger;
 	
-	public LoggerManager() {
-		this.configLoader = new ConfigurationLoader();
-	}
+	
+	public LoggerManager() { }
 	
 	public void loadConfiguration(String configFilePath) throws IOException {
-		this.configLoader.loadConfiguration(configFilePath);
-		this.initializeFormatter();
-		this.initializeLogger();
+		ConfigurationLoader configLoader = new ConfigurationLoader();
+		configLoader.loadConfiguration(configFilePath);
+		Formatter formatter = this.initializeFormatter(configLoader);
+		this.initializeLogger(configLoader, formatter);
 	}
 	
 	public Logger getLogger() {
 		return this.logger;
 	}
 	
-	private void initializeFormatter() {
-		String format = this.configLoader.getMessageFormat();
-		String separator = this.configLoader.getMessageSeparator();
+	private Formatter initializeFormatter(ConfigurationLoader configLoader) {
+		Formatter formatter;
+		String format = configLoader.getMessageFormat();
+		String separator = configLoader.getMessageSeparator();
 		if (separator != null && !separator.isEmpty()) {
-			this.formatter = new SimpleFormatter(format, separator);
+			formatter = new SimpleFormatter(format, separator);
 		} else {
-			this.formatter = new SimpleFormatter(format);
+			formatter = new SimpleFormatter(format);
 		}
+		return formatter;
 	}
 
-	private void initializeLogger() throws IOException {
-		this.logger = new Logger(this.formatter, this.configLoader.getLevel());
-		if (this.configLoader.getLogToConsole()) {
+	private void initializeLogger(ConfigurationLoader configLoader, Formatter formatter) throws IOException {
+		this.logger = new Logger(formatter, configLoader.getLevel());
+		if (configLoader.getLogToConsole()) {
 			this.logger.addConsoleOutput();
 		}
-		String[] fileOutputs = this.configLoader.getLogToFiles();
+		String[] fileOutputs = configLoader.getLogToFiles();
 		if (fileOutputs != null) {
 			for (int i = 0; i < (fileOutputs.length - 1); i++) {
 				this.logger.addFileOutput(fileOutputs[i]);
