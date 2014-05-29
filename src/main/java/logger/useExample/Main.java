65427ff4fe40c6984af177e2502c6a3c9904c6b4
AndresSanabria <andresg.sanabria@gmail.com>
2014-05-28 23:32:58 -0300

    Delete print that was not needed

--- src/main/java/logger/useExample/Main.java
+++ src/main/java/logger/useExample/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	
 	public static void main(String [] args)
 	{
-		System.out.println("current dir = " + System.getProperty("user.dir"));
 		Logger logger = new Logger("configFiles/config.properties");
 		logger.debug("This message will not be logged because the level is info");
 		logger.error("This is an example about how to use Logger");	
