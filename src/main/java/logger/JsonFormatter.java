37cdb78669d7f9121a59043cecf8bd8bcea4f374
AndresSanabria <andresg.sanabria@gmail.com>
2014-06-04 18:38:34 -0300

    JsonFormatter

---
+++ src/main/java/logger/JsonFormatter.java
@@ -0,0 +1,46 @@
+package logger;
+
+import net.sf.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * The Class JsonFormatter give json format to messages.
+ */
+public class JsonFormatter implements Formatter{
+	
+	/** The logger name. */
+	private String loggerName; 
+	
+	/**
+	 * Instantiates a new json formatter.
+	 *
+	 * @param loggername the logger name
+	 */
+	public JsonFormatter(String loggername) {
+		this.loggerName = loggername;
+	}
+
+	@Override
+	public String giveFormat(Level level, String logMsg) {
+		JSONObject json = new JSONObject();
+		json.element("datetime",getDate());
+		json.element("level", level.getName());
+		json.element("logger",loggerName);
+		json.element("message", logMsg);
+		return json.toString();
+	}
+	
+	/**
+	 * Gets the actual date.
+	 *
+	 * @return the date
+	 */
+	private String getDate() {
+		Date date = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		return dateFormat.format(date);
+	}
+
+}
