bfa2f35c03fdb629655998122b08fa65f4d94b3a
AndresSanabria <andresg.sanabria@gmail.com>
2014-06-05 09:49:32 -0300

    Missing Javadoc

--- src/main/java/logger/customFactory/CustomFactory.java
+++ src/main/java/logger/customFactory/CustomFactory.java
@@ -6,53 +6,18 @@ import java.lang.reflect.InvocationTargetException;
 import logger.filters.Filterer;
 import logger.writables.Writable;
 
+/**
+ * A factory for creating Custom objects.
+ */
 public class CustomFactory {
 	
-//	public Filterer createCustomFilter(String implementor, String[] params) throws CustomFilterException {
-//		Class<?> customClass;
-//		try {
-//			customClass = Class.forName(implementor);
-//			if (!implementsFilterer(customClass)) {
-//				throw new CustomFilterException();
-//			}
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//			throw new CustomFilterException();
-//		}
-//		Constructor<?>[] allConstructors;
-//		Constructor<?> constructor = null;
-//		try {
-//			allConstructors = customClass.getDeclaredConstructors();
-//			for (Constructor<?> oneConstructor: allConstructors) {
-//				if (oneConstructor.getParameterTypes().length == params.length) { // assuming types are correct
-//					constructor = oneConstructor;
-//				}
-//			}
-//		} catch (SecurityException e1) {
-//			e1.printStackTrace();
-//			throw new CustomFilterException();
-//		}
-//		Filterer customFilter;
-//		try {
-//			customFilter = (Filterer)constructor.newInstance((Object[])params);
-//		} catch (InstantiationException | IllegalAccessException
-//				| IllegalArgumentException | InvocationTargetException e) {
-//			e.printStackTrace();
-//			throw new CustomFilterException();
-//		} 
-//		return customFilter;
-//	}
-	
-//	private boolean implementsFilterer(Class<?> oneClass) {
-//		Class<?>[] interfacesImplemented = oneClass.getInterfaces();
-//		for (Class<?> oneInterface: interfacesImplemented) {
-//			if (oneInterface.getName().equals(Filterer.class.getName())) {
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-	
+	/**
+	 * Check if the Custom Object can be implemented.
+	 *
+	 * @param oneClass the class generated with the String Implementor
+	 * @param customClass the custom class for the method createCustom
+	 * @return true, if Custom Object can be implemented
+	 */
 	private boolean implementsCustom(Class<?> oneClass,Class<?> customClass) {
 		Class<?>[] interfacesImplemented = oneClass.getInterfaces();
 		for (Class<?> oneInterface: interfacesImplemented) {
@@ -63,6 +28,14 @@ public class CustomFactory {
 		return false;
 	}
 	
+	/**
+	 * Creates a new Custom object.
+	 *
+	 * @param implementor Name of the object class to create
+	 * @param params the parameters of the constructor of the class to create 
+	 * @param customClassToCreate the custom class to create
+	 * @return the custom object
+	 */
 	private Object createCustom(String implementor, String[] params,Class<?> customClassToCreate){
 		Class<?> customClass;
 		try {
@@ -98,6 +71,14 @@ public class CustomFactory {
 		return custom;
 	}
 	
+	/**
+	 * Creates a new Custom object.
+	 *
+	 * @param implementor Name of the object class to create
+	 * @param params the parameters of the constructor of the class to create 
+	 * @return the filterer
+	 * @throws CustomFilterException the custom filter exception
+	 */
 	public Filterer createCustomFilter(String implementor, String[] params) throws CustomFilterException {
 		Class<Filterer> customClassToCreate = Filterer.class;
 		Filterer custom = customClassToCreate.cast(createCustom(implementor, params, customClassToCreate));
@@ -107,6 +88,14 @@ public class CustomFactory {
 		return custom;
 	}
 	
+	/**
+	 * Creates a new Custom object.
+	 *
+	 * @param implementor Name of the object class to create
+	 * @param params the parameters of the constructor of the class to create 
+	 * @return the writable object
+	 * @throws CustomOutputException the custom output exception
+	 */
 	public Writable createCustomOutput(String implementor, String[] params) throws CustomOutputException {
 		Class<Writable> customClassToCreate = Writable.class;
 		Writable custom = customClassToCreate.cast(createCustom(implementor, params, customClassToCreate));
